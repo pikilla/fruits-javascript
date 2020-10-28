a68e1ba9cc43b8a6f0e3a4f5d093b9bb9030d124
pikilla <maite.lajournade@gmail.com>
2020-10-28 16:51:07 +0100

    fruits debut

---
+++ exo.js
@@ -0,0 +1,4 @@
+var arrayFruits=["pomme", "pastèque","mangue", "groseille","fruit de la passion"];
+var fruit={nom:"fraise", couleur:"rouge", forme:"rond", goût:"très sucré"};
+console.log(fruit)
+console.log(arrayFruits[1])
